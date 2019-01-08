12a17ce87d6d1e7f111fe76ba87b02c61d4f71a4
unesgit <unes82@gmail.com>
2019-01-08 16:21:06 +0000

    adding activeMQ

--- BackOffice/demo/src/main/java/com/example/demo/DemoApplication.java
+++ BackOffice/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import javax.jms.Queue;
+
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 
 /**
  * @author y.nadir
@@ -12,7 +17,13 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableJms
 public class DemoApplication {
+    
+    @Bean
+    public Queue queue() {
+        return new ActiveMQQueue("sample.queue");
+    }
     
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
